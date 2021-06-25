c752499c0563eafa82378a2ff4925afc362729aa
Pedro Ceglia <82342272+PedroCeglia@users.noreply.github.com>
2021-06-25 17:32:57 -0300

    Update YoutubeConfig.java

--- app/src/main/java/com/youtube/app/clone/curso/android/pedro/youtube/helper/YoutubeConfig.java
+++ app/src/main/java/com/youtube/app/clone/curso/android/pedro/youtube/helper/YoutubeConfig.java
@@ -3,7 +3,7 @@ package com.youtube.app.clone.curso.android.pedro.youtube.helper;
 public class YoutubeConfig {
 
     public static String CANAL_ID = "UC5k4fsA9AhxNLgMQa36CVdg";
-    public static String  CHAVE_YOUTUBE_API = "AIzaSyCErrVWKCvQuYZTZ7lzyehFnTWU6CucIOw";
+    public static String  CHAVE_YOUTUBE_API = "PESSOAL";
     public static String URL_BASE = "https://www.googleapis.com/youtube/v3/";
 
 }
